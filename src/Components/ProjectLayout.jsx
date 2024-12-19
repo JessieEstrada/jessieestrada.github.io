11d0556401e6b61cbf164e4f1311f7995f7f4dd6
Jessie Estrada <jessie@Mac.attlocal.net>
2024-12-18 22:53:29 -0800

    project section changes

--- src/Components/ProjectLayout.jsx
+++ src/Components/ProjectLayout.jsx
@@ -6,24 +6,24 @@ const ProjectLayout = () => {
   return (
     <div className="projects-container">
       <ProjectCard
-        image="./images/github-mark.jpeg"
-        title="Project 1"
-        description="Description for project 1."
-        techStack={["React", "JavaScript", "CSS"]}
-        githubLink="https://github.com/project1"
+        image="./images/WormHole-Gaming-Screenshot.png"
+        title="WormHole Gaming"
+        description="Explore our front-end application for the Game Store API. Features include easy navigation, checkout, addition of new items, and display of all items. Striving for a user-friendly experience, high performance, and secure transactions."
+        techStack={["Java", "SpringBoot", "AWS", "GraphQL", "JWT", "Thymeleaf", "MySQL", "SQL", "CircleCI", "DBeaver", "Docker"]}
+        githubLink="https://github.com/JessieEstrada/Game-Store-Complete-App"
       />
       <ProjectCard
-        image="/path-to-image2.jpg"
-        title="Project 2"
-        description="Description for project 2."
-        techStack={["Node.js", "Express", "MongoDB"]}
-        githubLink="https://github.com/project2"
+        image="./images/Bookstore-Inventory System-Screenshot.png"
+        title="Bookstore Invntory System API"
+        description="This Bookstore Inventory System REST API manages inventory for any bookstore! Our development team has successfully crafted an extensive and entirely operational API designed to emulate the functionalities of a bookstore inventory system. Our users would be able to create, read, update, and delete: books, publishers, and authors. By employing the capabilitites of Spring Boot JPA and Java, we have constructed models, controllers, repositories, a GraphQL, and comprehensive testing to ensure functionality of the API."
+        techStack={["Java", "SpringbBoot", "GraphQL", "DBeaver", "Docker", "MySQL", "SQL"]}
+        githubLink="https://github.com/JessieEstrada/Bookstore-Inventory"
       />
       <ProjectCard
-        image="/path-to-image3.jpg"
-        title="Project 3"
-        description="Description for project 3."
-        techStack={["Python", "Django", "PostgreSQL"]}
+        image="./images/Doggie-Dashboard-Screenshot.png"
+        title="Doggie Dash"
+        description="This webpage accesses a public API and constructs a dashboard view of the data. The dashboard view contains summary statistics and a list view of the data that can be searched and filtered. In specific, this website uses the Petfinder API by Purina to provide a dashboard view of different dogs available for adoption."
+        techStack={["React", "HTML", "CSS", "Javascript"]}
         githubLink="https://github.com/project3"
       />
       <ProjectCard
