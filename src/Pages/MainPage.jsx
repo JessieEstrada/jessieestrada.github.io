138f3f3b97df9d96b5d7817fa91e8eb0365a1a64
Jessie Estrada <jestrada3745@gmail.com>
2025-08-07 08:49:46 -0700

    small changes

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -10,7 +10,7 @@ const MainPage = () => {
       <div className="main-page">
         <IntroSection
           title={"Software Engineer | Data & IT Specialist"}
-          resumeLink="https://docs.google.com/document/d/1RKx5oyX2uIYIMzN6CcOdRUOi5ZSn37auO2WwIxOYT3I/edit?usp=sharing"
+          resumeLink="https://docs.google.com/document/d/1dfPfVW7S_IWGmed_gEMelEAtL_J0_k6LeHvIImODBu0/edit?usp=sharing"
         />
       </div>
     </>
